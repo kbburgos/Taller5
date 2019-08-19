19d2d3e97b728555a0af27a74a9fa97409f5008a
kbburgos <43821172+kbburgos@users.noreply.github.com>
2019-08-19 14:45:50 -0500

    Update Script

--- taller5Script.sql
+++ taller5Script.sql
@@ -1,5 +1,7 @@
 create database GoodReads;
 use GoodReads;
+
+
 create table Usuario(
 	cedula varchar(10) primary key not null,
     nombre varchar(50),
@@ -14,14 +16,22 @@ insert into Usuario values("0910734215", "Marishka", "Bueno", "mbueno@gmail.com"
 insert into Usuario values("0915567415", "Critopher", "Melloni", "cmelloni@gmail.com","1980-05-05");
 insert into Usuario values("0910088796", "Dik", "Wolf", "aleavalos@gmail.com","1996-07-07");
 
-
-create table Libro(
-	isbn varchar(15) primary key not null,
-    titulo varchar(100),
-    autor varchar(50)
-    );
-    
 create table Autor(
 	id int primary key auto_increment,
     nombre_completo varchar(120)
+    );
+    
+create table Libro(
+	isbn varchar(15) primary key not null,
+    titulo varchar(100),
+    autor int,
+    FOREIGN KEY (autor) REFERENCES Autor(id)
+    );
+    
+create table Calificacion(
+	id int primary key auto_increment,
+    usuario varchar(10),
+    libro varchar(15),
+    FOREIGN KEY (libro) REFERENCES Libro(isbn),
+    FOREIGN KEY (usuario) REFERENCES Usuario(cedula)
     );
