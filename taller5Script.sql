e5d79479c2dd732899f96f41fc36404147f6f0a5
kbburgos <43821172+kbburgos@users.noreply.github.com>
2019-08-19 14:34:11 -0500

    Script DataBase

---
+++ taller5Script.sql
@@ -0,0 +1,27 @@
+create database GoodReads;
+use GoodReads;
+create table Usuario(
+	cedula varchar(10) primary key not null,
+    nombre varchar(50),
+    apellido varchar(50),
+    correo varchar(50),
+    fechaNacimiento date
+    );
+    
+insert into Usuario values("0910786615", "Alejandra", "Avalos", "aleavalos@gmail.com","1996-05-05");
+insert into Usuario values("0910786677", "Travis", "Maddox", "tmaddox@gmail.com","1978-05-05");
+insert into Usuario values("0910734215", "Marishka", "Bueno", "mbueno@gmail.com","2000-08-12");
+insert into Usuario values("0915567415", "Critopher", "Melloni", "cmelloni@gmail.com","1980-05-05");
+insert into Usuario values("0910088796", "Dik", "Wolf", "aleavalos@gmail.com","1996-07-07");
+
+
+create table Libro(
+	isbn varchar(15) primary key not null,
+    titulo varchar(100),
+    autor varchar(50)
+    );
+    
+create table Autor(
+	id int primary key auto_increment,
+    nombre_completo varchar(120)
+    );
